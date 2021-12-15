d01fb3709fbcf5c3778c4374e1eab8d673649834
Faryan Rezagholi <faryan.rezagholi@siedle.de>
2021-12-16 00:23:42 +0100

    remove set/check for large address aware binary after build

--- Tools/postbuild_mremoteng.ps1
+++ Tools/postbuild_mremoteng.ps1
@@ -27,7 +27,7 @@ param (
 
 Write-Output "+===========================================================================================+"
 Write-Output "|                               Beginning mRemoteNG Post Build                              |"
-Write-Output "+===========================================================================================+"
+Write-Output "+-------------------------------------------------------------------------------------------+"
 
 Format-Table -AutoSize -Wrap -InputObject @{
     "SolutionDir" = $SolutionDir
@@ -54,13 +54,11 @@ Format-Table -AutoSize -Wrap -InputObject @{
 # }
 ###
 
-& "$PSScriptRoot\set_LargeAddressAware.ps1" -TargetDir $TargetDir -TargetFileName $TargetFileName
-& "$PSScriptRoot\verify_LargeAddressAware.ps1" -TargetDir $TargetDir -TargetFileName $TargetFileName
 & "$PSScriptRoot\tidy_files_for_release.ps1" -TargetDir $TargetDir -ConfigurationName $ConfigurationName
 & "$PSScriptRoot\sign_binaries.ps1" -TargetDir $TargetDir -CertificatePath $CertificatePath -CertificatePassword $CertificatePassword -ConfigurationName $ConfigurationName -Exclude $ExcludeFromSigning -SolutionDir $SolutionDir
 & "$PSScriptRoot\verify_binary_signatures.ps1" -TargetDir $TargetDir -ConfigurationName $ConfigurationName -CertificatePath $CertificatePath -SolutionDir $SolutionDir
 & "$PSScriptRoot\zip_files.ps1" -SolutionDir $SolutionDir -TargetDir $TargetDir -ConfigurationName $ConfigurationName
 
-Write-Output "+===========================================================================================+"
+Write-Output "+-------------------------------------------------------------------------------------------+"
 Write-Output "|                                  End mRemoteNG Post Build                                 |"
 Write-Output "+===========================================================================================+"
