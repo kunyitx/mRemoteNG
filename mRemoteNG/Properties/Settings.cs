07120c8ab800928ced5770ff483985ddcf9b5b95
Faryan Rezagholi <faryan.rezagholi@siedle.de>
2021-12-16 18:51:31 +0100

    changed json format

---
+++ mRemoteNG/Properties/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mRemoteNG.Properties
+{
+    internal class Settings
+    {
+        public Settings()
+        {
+            var configuration = new Configuration("AppSettings");
+            var appSettings = configuration.Get("AppSettings"); // null
+            var token = configuration.Get("token"); // null
+        }
+    }
+}
